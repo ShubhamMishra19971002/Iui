a2097c3655b13b41ebf6cbf5c80018721c4a575b
Tanushree Soni <108271833+TanushreeSoni2021@users.noreply.github.com>
2023-06-27 21:45:48 +0530

    Brand manager

---
+++ src/Modules/BrandManagers/Views/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BrandHomeScreen from './BrandHomeScreen'
+const Views = (props: any) => {
+    console.log(props.hook)
+  return (
+    <>
+        <BrandHomeScreen />
+        
+    </>
+  )
+}
+
+export default Views
--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Registaration from './Modules/Registration/Registaration'
+import BrandHomeScreen from './Modules/BrandManagers/Views/BrandHomeScreen'
 
 const Routing = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/signup"  element={<Registaration/>}>
+      <Route path="/brandDashboard"  element={<BrandHomeScreen />}>
+
       </Route>
     </Routes>
     </BrowserRouter>    
